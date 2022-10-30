7adad196c3c813ab2b16a66e4e231d7b8c35fe8d
twodollars123 <twodollars.no2002@gmail.com>
2022-10-30 15:51:40 +0700

    Wrapper Popper Tooltip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import "./App.scss";
 import Button from "./components/Button/Button";
+import Wrapper from "./components/Wrapper/Wrapper.js";
+
+import Tippy from "@tippyjs/react/headless";
+import "tippy.js/dist/tippy.css";
+import { useState } from "react";
 
 function App() {
+  const [show, setShow] = useState(false);
+  const hide = () => {
+    setShow(false);
+  };
   return (
     <div className="App">
-      <Button primary rightIcon={<i className="fa fa-address-book" />}>
-        abc
-      </Button>
-      <Button text>a</Button>
+      <Tippy
+        interactive
+        visible={show}
+        onClickOutside={hide}
+        render={(attrs) => (
+          <div className="box" tabIndex="-1" {...attrs}>
+            <Wrapper>
+              <h2>abc</h2>
+            </Wrapper>
+          </div>
+        )}
+      >
+        <div>
+          <Button
+            primary
+            rightIcon={<i className="fa fa-address-book" />}
+            onClick={() => setShow(true)}
+          >
+            abc
+          </Button>
+        </div>
+      </Tippy>
     </div>
   );
 }
